72189f159f6d9b5de10d95973f66b2751035f48a
satchmo2002 <satchwill.aaron@gmail.com>
2017-06-12 04:26:30 +0000

    Made page implimented all except gap at bottom

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  
+  root 'home#home'
+  get 'about', to: 'home#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
